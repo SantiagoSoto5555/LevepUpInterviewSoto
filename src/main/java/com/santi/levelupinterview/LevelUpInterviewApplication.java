ca475ac28258cf915ad8b7223d6bf3bc86527839
SantiagoSoto5555 <sotosantiago222@gmail.com>
2025-06-08 17:27:12 -0300

    first test

--- src/main/java/com/santi/levelupinterview/LevelUpInterviewApplication.java
+++ src/main/java/com/santi/levelupinterview/LevelUpInterviewApplication.java
@@ -8,6 +8,7 @@ public class LevelUpInterviewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LevelUpInterviewApplication.class, args);
+        a
     }
 
 }
